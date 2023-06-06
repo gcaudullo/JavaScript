a963184a309ad7e2e4ee9021627ea75e5e712fec
Gonzalo Caudullo <gonchicaudullo@gmail.com>
2023-06-05 21:49:50 -0300

    Agrego Objetos y arrays de Alumnos

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
+class Alumno {
+    constructor(nombreCompleto, nota) {
+        this.nombreCompleto = nombreCompleto;
+        this.nota = nota;
+    }
 
-function solicitoNombre(){
+    getNombreCompleto() {
+        return this.nombreCompleto;
+    }
+
+    getNota() {
+        return this.nota;
+    }
+
+    setNombreCompleto(nuevoNombre) {
+        this.nombreCompleto = nuevoNombre;
+    }
+
+    setNota(nuevaNota) {
+        this.nota = nuevaNota;
+    }
+}
+
+
+function solicitoNombre() {
     let nombreCompleto = prompt("Ingrese Nombre y Apellido o de lo contrario ingrese Fin: ");
     return (nombreCompleto)
 }
 
-function solicitoNota(){
+function solicitoNota() {
     let nota = parseInt(prompt("Ingrese Nota del alumno: "));
     return (nota)
 }
 
-function dividir(dividendo, divisor){
-    return dividendo/divisor;
+function dividir(dividendo, divisor) {
+    return dividendo / divisor;
 }
 
 alert("Este programa calcula el promedio de notas de una clase \nPara ello ingrese nombre completo y nota adquirida por cada alumno \nPara finalizar el listado escriba Fin");
+const alumnos = [];
 let nombreCompleto = solicitoNombre();
-let i=0;
-let sumatoriaNotas=0;
-let nota=0;
-while(nombreCompleto!="Fin"){
-    nota =solicitoNota()
-    if(nota>12 || nota<1) {
+let i = 0;
+let sumatoriaNotas = 0;
+let nota = 0;
+while (nombreCompleto != "Fin") {
+    nota = solicitoNota()
+    if (nota > 12 || nota < 1) {
         alert("nota fuera del rango permitido 1..12");
     }
-    else{
-        sumatoriaNotas= sumatoriaNotas + nota;
+    else {
+        alumnos[i] = new Alumno(nombreCompleto, nota)
+        sumatoriaNotas = sumatoriaNotas + nota;
         nombreCompleto = solicitoNombre();
-        i+=1;
+        i += 1;
     }
 }
 alert("La clase cuenta con " + i + " alumnos");
-alert("El promedio de notas de la clase es: " + dividir(sumatoriaNotas,i));
+alert("los alumnos ingresados y sus notas son: ")
+for (let j = 0; j < alumnos.length; j++) {
+    alert(alumnos[j].getNombreCompleto() + " " + alumnos[j].getNota())
+}
+alert("El promedio de notas de la clase es: " + dividir(sumatoriaNotas, i));
